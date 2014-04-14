292ca90b0ccf4ff5c5ddaf270c8537122caf0c5a
DawJoanIvars <joan_ic_92@hotmail.com>
2014-04-14 10:14:13 +0200

    Correjid.o

--- ArraysII/src/IslaDesierta.java
+++ ArraysII/src/IslaDesierta.java
@@ -6,26 +6,25 @@ public class IslaDesierta {
 
 		//Creamos el array.
 		String equipaje[]= new String[10];
-	
-		//Lo iniciamos.
-		for(int i=1;i<equipaje.length;i++){
-			
-			//Le damos un valor a cada uno.
-			equipaje[0]="Agua";
-			equipaje[1]="Comida";
-			equipaje[2]="Smarthphone de gama alta a poder ser";
-			equipaje[3]="Pistola con municion";
-			equipaje[4]="Cuchillos";
-			equipaje[5]="Cuerdas";
-			equipaje[6]="Brujula";
-			equipaje[7]="Radio";
-			equipaje[8]="Ropa";
-			equipaje[9]="Tienda de campaña";
-			
+		
+		//Le damos un valor a cada uno.
+		equipaje[0]="Agua";
+		equipaje[1]="Comida";
+		equipaje[2]="Smarthphone de gama alta a poder ser";
+		equipaje[3]="Pistola con municion";
+		equipaje[4]="Cuchillos";
+		equipaje[5]="Cuerdas";
+		equipaje[6]="Brujula";
+		equipaje[7]="Radio";
+		equipaje[8]="Ropa";
+		equipaje[9]="Tienda de campaña";
+		int j=0;
+		for(int i=0;i<equipaje.length;i++){
 			//Vemos el resultado.
-			System.out.println("A una isla me llevaria: "+equipaje[i]);
+			j=i+1;
+			System.out.println("A una isla me llevaria: "+j+"."+equipaje[i]);
 			
 		}
+	
 	}
-
 }
